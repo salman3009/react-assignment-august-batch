9e0afecdb2f0dea0493643bff1d8c890049f75b5
salman3009 <salman444g@gmail.com>
2023-03-19 12:40:46 +0530

    code

--- August/18-03-23/basic-assignment/src/App.js
+++ August/18-03-23/basic-assignment/src/App.js
@@ -1,4 +1,4 @@
-import { useState,useRef } from "react";
+import { useState,useRef, useEffect } from "react";
 import Form  from "./Form";
 import List from "./List";
 
@@ -11,19 +11,28 @@ const list=[{
 const App=()=>{
 
        const [getList,setList] = useState(list);
-
        const firstDate = useRef(null);
        const SecondDate = useRef(null);
+
+       useEffect(()=>{
+           firstDate.current.id = "newDate";
+       },[])
 
        const appendDetails=(obj)=>{
             setList([...getList,obj]);
        } 
 
        const calculateDate=()=>{
+              console.log(firstDate.current.name);
             let first = new Date(firstDate.current.value);
             let second = new Date(SecondDate.current.value);
-            let result = Math.abs(second-first);
-            let finalResult = Math.ceil((result/(1000*60*60*24)));
+            let result = Math.abs(second-first);//milliseconds
+            let finalResult = Math.ceil((result/(1000*60*60*24)));//no of days
+            //1000 - 1000 milliseconds = 1seconds 
+            //60 - seconds 
+            //60 - menutes 
+            //24 - hours
+            //no of days
             console.log(finalResult);
        }
 
@@ -36,7 +45,7 @@ const App=()=>{
         {list.length && (<div>
               <h1>Hello world</h1>
         </div>)}
-        First Date:<input type="date" ref={firstDate}/>
+        First Date:<input type="date" name ="fullDate"  ref={firstDate}/>
         second Date:<input type="date" ref={SecondDate}/>
         <button onClick={calculateDate}>Date Function</button>
        </div>)
