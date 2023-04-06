899caa7b6c76bc1b56d9544eeeca6a281b1012d1
salman3009 <salman444g@gmail.com>
2023-04-06 19:39:52 +0530

    code

--- August/Projects/basic-authentication-redux/src/Dashboard.js
+++ August/Projects/basic-authentication-redux/src/Dashboard.js
@@ -1,24 +1,29 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
+import { initialProductList } from "./reducers/productSlice";
 
 const Dashboard = () => {
 
-    const authentication = useSelector((state)=>state.auth.authentication);
+    const authentication = useSelector((state) => state.auth.authentication);
+    const list = useSelector((state) => state.product.productList);
+    const dispatch = useDispatch();
     const navigation = useNavigate();
 
-    useEffect(()=>{
-        let result = authentication?authentication:Boolean(sessionStorage.getItem('authentication'));
-        if(!result){
-          navigation('/');
+    useEffect(() => {
+        let result = authentication ? authentication : Boolean(sessionStorage.getItem('authentication'));
+        if (!result) {
+            navigation('/');
         }
 
-    },[authentication])
+    }, [authentication])
 
-    useEffect(()=>{
-
-    })
+    useEffect(() => {
+        axios.get('http://localhost:3000/productList').then((result) => {
+            dispatch(initialProductList(result.data));
+        })
+    }, [])
 
     return (<div className="row">
         <div className="col-2"></div>
@@ -34,13 +39,20 @@ const Dashboard = () => {
                     </tr>
                 </thead>
                 <tbody>
-                   
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
+                    {
+                        list.map((obj, index) => {
+                            return (
+                                <tr key={index}>
+                                    <th scope="row">{index+1}</th>
+                                    <td>{obj.name}</td>
+                                    <td>{obj.price}</td>
+                                    <td>{obj.price}</td>
+                                    <td><button>Delete</button></td>
+                                </tr>
+                            )
+                        })
+                    }
+
                 </tbody>
             </table>
         </div>
