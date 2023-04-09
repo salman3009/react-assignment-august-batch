492022591a48f0c5bb049d9d6182a005079f1560
salman3009 <salman444g@gmail.com>
2023-04-09 16:07:14 +0530

    code

--- August/Projects/basic-class/src/App.js
+++ August/Projects/basic-class/src/App.js
@@ -1,31 +1,52 @@
 import { Component } from "react";
 import Form from "./Form";
 import List from "./List";
-
+import axios from "axios";
 
 class App extends Component {
 
+  //initialization method
   constructor(props){
+   //alert("constructor 1")
+    //always pass it so derived class can access props
     super(props)
+    //we have only one state in entire class component
     this.state={
       flag:false,
       productName:[]
     }
   }
 
+  componentDidMount(){
+    //alert("componentDidMount 3")
+    //initial load of the component
+    axios.get('http://localhost:3000/list').then((result)=>{
+          this.setState({productName:result.data?result.data:[]})
+    })
+  }
+
   productUpdateHandler=(input)=>{
-    this.setState({productName:[...this.state.productName,input]})
+    axios.post('http://localhost:3000/list',{name:input}).then(()=>{
+      this.setState({productName:[...this.state.productName,input]})
+    })
+   
   }
 
   componentDidUpdate(){
+   // alert("componentDidUpdate 4")
+    //for every state updates
      console.log(this.state.productName);
   }
   
 
+  
   render(){
+    //alert("render 2")
+    //to render the jsx mandatory to use it
+    //we need to use this keyword for all methods and functions
     return (<div>
       <Form updateProduct={this.productUpdateHandler}/>
-      <List/>
+      <List product={this.state.productName}/>
     </div>)
   }
 }
--- August/Projects/basic-class/src/List.js
+++ August/Projects/basic-class/src/List.js
@@ -2,9 +2,20 @@ import { Component } from "react";
 
 
 class List extends Component{
+
+    constructor(props){
+        super(props);
+    }
      
     render(){
-        return (<div>List</div>)
+        return (<div>
+            {this.props.product.map((obj,index)=>{
+                return (<div key={index}>
+                    <h1>{obj.name}</h1>
+                </div>)
+
+            })}
+        </div>)
     }
 }
 export default List;
