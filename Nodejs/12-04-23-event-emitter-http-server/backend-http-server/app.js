5928c94853ee4f3c68c9c601e4ddce0c2e66970d
salman3009 <salman444g@gmail.com>
2023-04-12 19:57:08 +0530

    code

---
+++ Nodejs/12-04-23-event-emitter-http-server/backend-http-server/app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+     res.writeHead(200,{'Content-Type':'text/html'});
+     res.end("<h1>Hello world</h1>");
+});
+
+module.exports = server;
+
