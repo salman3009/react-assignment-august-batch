850b3fd2059af94f433bbde7d77f1fcd2bb99c33
salman3009 <salman444g@gmail.com>
2023-04-14 19:56:43 +0530

    code

---
+++ Nodejs/14-04-23-express-nodemon-body-parser-get-post-middelware/basic-express/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
+
+app.use(parser.json());
+
+const productList=[{
+    id:1,
+    name:"car"
+}]
+
+
+app.use((req,res,next)=>{
+    console.log("middeware1");
+    next();
+
+})
+
+app.use((req,res,next)=>{
+    console.log("middeware 2 ");
+    next();
+})
+
+app.get('',(req,res)=>{
+    res.send("<h1>hello world newton</h1>");
+})
+
+app.get('/list',(req,res)=>{
+    res.status(400).json({
+        message:"success",
+        data:productList
+    })
+})
+
+app.post('/list',(req,res)=>{
+     console.log(req.body);
+     res.status(201).json({
+        message:"created"
+     })
+})
+
+
+app.listen(3000,()=>{
+    console.log("express is running on",3000);
+})
