20b6c0185a49f2329dd1e191ab2bf2aae357ad4d
salman3009 <salman444g@gmail.com>
2023-04-16 15:06:37 +0530

    code

--- Nodejs/16-04-23-API/TourAPI/app.js
+++ Nodejs/16-04-23-API/TourAPI/app.js
@@ -2,9 +2,24 @@ const express = require('express');
 const app = express();
 const parser = require('body-parser');
 app.use(parser.json());
+const fs  = require('fs');
+
+
+const list = JSON.parse(fs.readFileSync(`${__dirname}/data/tour.json`));
+console.log(list);
 
 app.get('',(req,res)=>{
-    res.status(200).send("<h1>hello world</h1>");
+    // res.status(200).send("<h1>hello world</h1>");
+    // res.status(200).send({product:"ac",price:2000});
+    //   res.status(200).json({
+    //     message:"success",
+    //     data:{product:"ac",price:2000}
+    //   })
+    //    res.status(404).end();
+     res.status(200).json({
+        data:list
+     })
+
 })
 
 
