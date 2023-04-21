d2e9364dcf5ab3ede7d86ff306de3084fb766ee0
salman3009 <salman444g@gmail.com>
2023-04-21 20:12:51 +0530

    code

--- Nodejs/19-04-23-servicing-side-rendering/SSR-static-file/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/user');
-
-
-router.get('/registration',userController.registerNewUser);
-
-
-module.exports = router;
--- Nodejs/19-04-23-servicing-side-rendering/ecommerce-api-mvc/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-
-
-//custom middelware
-module.exports = (req,res,next)=>{
-     req.token = true;
-     console.log("coming to middleware 3");
-     next();
-}
