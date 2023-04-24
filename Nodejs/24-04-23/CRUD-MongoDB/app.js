76a2677cb1f20120aaf0a4f341a63b2a3969d905
salman3009 <salman444g@gmail.com>
2023-04-24 21:20:43 +0530

    code

--- Nodejs/24-04-23/CRUD-MongoDB/app.js
+++ Nodejs/24-04-23/CRUD-MongoDB/app.js
@@ -3,11 +3,12 @@ const mongoClient = require('mongodb').MongoClient;
 const uri = "mongodb://localhost:27017/"
 const connect = new mongoClient(uri);
 const db = connect.db('augustNewton');
+const objectId = require('mongodb').ObjectId;
 
 async function insert()
 {
     try{
-        let result = await db.collection('employee').insertOne({status:true});
+        let result = await db.collection('employee').insertOne({firstName:"akash",age:56});
         console.log(result);
     }catch(err){
       console.log(err);
@@ -25,4 +26,18 @@ async function find(){
         console.log(err);
     }
 }
-find();
+// find();
+
+
+async function query(){
+    try{
+        // let query ={firstName:"akash",age:36}
+        let query ={_id: new objectId('6446a4804f20759a64d3a2be')}
+        let result = await db.collection('employee').find(query).toArray();
+        console.log(result);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+query();
