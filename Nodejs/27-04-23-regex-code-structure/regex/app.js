802c687c735c318d10b6faacb6e9bcdcdea2b7f5
salman3009 <salman444g@gmail.com>
2023-04-27 20:19:45 +0530

    code

---
+++ Nodejs/27-04-23-regex-code-structure/regex/app.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Employee = require('./models/employee');
+
+
+
+const uri = "mongodb://localhost:27017/augustslack";
+//const uri = "mongodb+srv://newton:nXN3TJ0SrJpmSChq@milesweb.xqz5h.mongodb.net/augustNewton";
+
+
+
+mongoose.connect(uri,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log("Database is connected");
+}).catch((err)=>{
+    console.log(err);
+})
+
+
+
+
+const findOperation = async ()=>{
+     try{
+        //projection
+        let result = await Employee.find({},{firstName:1,age:1,_id:0});
+        console.log(result);
+     }
+     catch(err){
+        console.log(err);
+     }
+}
+
+findOperation();
+
