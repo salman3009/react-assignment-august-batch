bcd292b2c3b9abe1c50958598e9417ebb34e3a94
salman3009 <salman444g@gmail.com>
2023-05-02 20:57:01 +0530

    code

---
+++ Nodejs/02-05-23-database-modelling/one-many-reference/app.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const Student = require('./models/student');
+const Address = require('./models/address');
+
+
+
+const uri = "mongodb://localhost:27017/databaseModelling";
+//const uri = "mongodb+srv://newton:nXN3TJ0SrJpmSChq@milesweb.xqz5h.mongodb.net/augustNewton";
+
+
+
+mongoose.connect(uri,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log("Database is connected");
+}).catch((err)=>{
+    console.log(err);
+})
+
+const createOperationStudent=async ()=>{
+    try{
+
+        const studentPost = new Student({
+             studentName:"akash",
+             studentId:"12MDD034",
+             branch:"CSE"
+        })  
+        const result = await studentPost.save();
+        console.log(result);
+    }
+    catch(err){
+      console.log(err);
+    }
+
+}
+
+// createOperationStudent();
+
+const createOperationAddress= async ()=>{
+  
+  try{
+
+    const addressPost = new Address({
+      permanentAddress:"4nd street",
+      city:"Bangalore",
+      pincode:"344532"
+ }) 
+
+    const result = await addressPost.save();
+    console.log(result);
+  }
+  catch(err){
+    console.log(err);
+  }
+
+}
+createOperationAddress();
+
+
+
+
+
---
+++ Nodejs/02-05-23-database-modelling/one-many-reference/models/address.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+//Declaring properties and datatypes
+const addressSchema = mongoose.Schema({
+                permanentAddress:{type:String},
+                city:{type:String},
+                pinCode:{type:Number}
+})
+//It should match proper data type 
+//Everything is optional above because we have not added validation properties
+//Any new property it will not take until we defined in schema
+
+//creating collection
+module.exports = mongoose.model('address',addressSchema);
