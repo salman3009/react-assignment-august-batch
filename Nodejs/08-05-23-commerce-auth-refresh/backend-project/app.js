b03da2b72b1e5b0ac5497df6e58e524771faa5f8
salman3009 <salman444g@gmail.com>
2023-05-08 22:03:26 +0530

    code

--- Nodejs/08-05-23-commerce-auth-refresh/backend-project/app.js
+++ Nodejs/08-05-23-commerce-auth-refresh/backend-project/app.js
@@ -5,9 +5,12 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 const authRoute = require('./routes/auth');
+const userRouter = require('./routes/user')
 
 
 app.use("/api/auth",authRoute);
+app.use("/api/user",userRouter);
+
 app.all("*",(req,res)=>{
   res.status(404).json({
     message:"please provide valid route"
