012c629fd11b3cc007c366cd41f09d0baccf8327
salman3009 <salman444g@gmail.com>
2023-05-08 20:59:12 +0530

    code

--- Nodejs/08-05-23-commerce-auth-refresh/backend-project/models/User.js
+++ Nodejs/08-05-23-commerce-auth-refresh/backend-project/models/User.js
@@ -3,17 +3,20 @@ const {Schema,model} = require('mongoose');
 const UserSchema = new Schema({
     name:{
         type:String,
+        trim:true,
         required:true,
         maxlength:[32,"maxlength is 32"]
     },
     email:{
         type:String,
+        trim:true,
         required:true,
         unique:true,
         lowercase:true
     },
     password:{
         type:String,
+        trim:true,
         required:true
     }
 });
---
+++ Nodejs/08-05-23-commerce-auth-refresh/backend-project/validators/auth.js
@@ -0,0 +1,12 @@
+const Joi = require('joi');
+
+const registerValidation = (data)=>{
+    const schema = Joi.object().keys({
+        name:Joi.string().min(4).required(),
+        email:Joi.string().min(6).required().email(),
+        password:Joi.string().min(6).required(),
+    });
+    return schema.validate(data);
+}
+
+module.exports.registerValidation = registerValidation;
