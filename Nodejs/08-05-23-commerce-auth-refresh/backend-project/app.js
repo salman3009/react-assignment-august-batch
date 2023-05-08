f5a3d0a1aa1c55494860ca14863ceae33cf89c38
salman3009 <salman444g@gmail.com>
2023-05-08 20:28:21 +0530

    code

---
+++ Nodejs/08-05-23-commerce-auth-refresh/backend-project/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended:true}));
+
+module.exports = app;
