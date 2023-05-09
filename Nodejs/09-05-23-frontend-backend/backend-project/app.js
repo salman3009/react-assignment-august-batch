92f0c5668e2049c8824f6bf09ed9ce0e676de6fa
salman3009 <salman444g@gmail.com>
2023-05-09 21:35:15 +0530

    code

--- Nodejs/09-05-23-frontend-backend/backend-project/app.js
+++ Nodejs/09-05-23-frontend-backend/backend-project/app.js
@@ -7,6 +7,7 @@ app.use(express.urlencoded({extended:true}));
 const authRoute = require('./routes/auth');
 const userRouter = require('./routes/user')
 
+
 app.use((req,res,next)=>{
    res.setHeader("Access-Control-Allow-Origin","*");
    next();
--- Nodejs/09-05-23-frontend-backend/frontend-project/src/App.js
+++ Nodejs/09-05-23-frontend-backend/frontend-project/src/App.js
@@ -1,5 +1,16 @@
 import './App.css';
 import  Routes  from './routes/Routes';
+
+import axios from 'axios';
+
+axios.interceptors.request.use((request)=>{
+     console.log("api call is going");
+     return request;
+},(error)=>{
+  console.log(error);
+  return Promise.reject(error);
+})
+
 function App() {
   return (
     <Routes/>
