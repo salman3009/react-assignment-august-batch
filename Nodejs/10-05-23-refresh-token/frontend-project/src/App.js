2934fc8bb8e4050a60606f29a33b6ab9a3a07bce
salman3009 <salman444g@gmail.com>
2023-05-10 21:18:02 +0530

    code

--- Nodejs/10-05-23-refresh-token/frontend-project/src/App.js
+++ Nodejs/10-05-23-refresh-token/frontend-project/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import  Routes  from './routes/Routes';
-
 import axios from 'axios';
+import jwt_decode from "jwt-decode";
+
 
 axios.interceptors.request.use((request)=>{
      console.log("api call is going");
-    request.headers['auth-token'] = sessionStorage.getItem('auth-token');
+     request.headers['auth-token'] = sessionStorage.getItem('auth-token');
+     if(!sessionStorage.getItem('auth-token')){
+      return request;
+     }
+    const {exp} = jwt_decode(sessionStorage.getItem('auth-token'));
+    const expDate = new Date(parseInt(exp)*1000);
+    const currentDate = new Date();
+    console.log(currentDate,expDate);
+    if(currentDate > expDate){
+      alert("trigger api");
+    }
+   
      return request;
 },(error)=>{
   console.log(error);
