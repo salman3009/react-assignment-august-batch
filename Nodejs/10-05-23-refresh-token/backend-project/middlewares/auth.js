571d715ceac9b74934e1aaab68ab041ba8a96454
salman3009 <salman444g@gmail.com>
2023-05-10 20:36:16 +0530

    code

--- Nodejs/10-05-23-refresh-token/backend-project/middlewares/auth.js
+++ Nodejs/10-05-23-refresh-token/backend-project/middlewares/auth.js
@@ -13,8 +13,17 @@ const verifyAuthToken = async (req,res,next)=>{
       })
     }
    
+}
 
-
+const verifyRefreshToken = (req,res,next)=>{
+  try{
+    const token = req.header("refresh-token");
+    const verified = jwt.verify(token,process.env.REFRESH_TOKEN_SECRET);
+    req.user = verified;
+    next();
+  }catch(err){
+    res.status(400).json({message:err.message});
+  }
 }
 
 module.exports.verifyAuthToken = verifyAuthToken;
