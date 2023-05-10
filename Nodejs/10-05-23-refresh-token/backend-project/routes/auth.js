13cbdd54d1d0b9d76e69c3f2208a042c346e3c99
salman3009 <salman444g@gmail.com>
2023-05-10 20:50:25 +0530

    code

--- Nodejs/10-05-23-refresh-token/backend-project/routes/auth.js
+++ Nodejs/10-05-23-refresh-token/backend-project/routes/auth.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    register,login,
+    register,login,generateNewauthToken
 } = require("../controllers/auth");
+const {verifyRefreshToken} = require('../middlewares/auth');
 
 router.post('/register',register);
 router.post('/login',login);
+router.get("/refresh",verifyRefreshToken,generateNewauthToken);
 
 module.exports = router;
