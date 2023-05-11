0c1335baf50100dad627755bca4cdb8feba960c2
salman3009 <salman444g@gmail.com>
2023-05-11 20:58:50 +0530

    code

--- Nodejs/11-05-23-error-handling/mongoose-error-handling/app.js
+++ Nodejs/11-05-23-error-handling/mongoose-error-handling/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Employee = require('./models/employee');
 
 mongoose.connect('mongodb://localhost:27017/errorHandling',{
    useNewUrlParser:true,
@@ -16,18 +17,20 @@ mongoose.connect('mongodb://localhost:27017/errorHandling',{
 
 app.post('',(req,res,next)=>{
   try{
-     let error = new Error("invalid data from frontend");
-     error.status = 500;
-     throw error;
+     const employeeResult = new Employee({
+       firstName:req.body.firstName,
+       age:req.body.age,
+       hobbies:req.body.hobbies
+     });
+     employeeResult.save().then((result)=>{
+       return res.status(201).send(result);
+     }).catch((err)=>{
+       next(err);
+     })
   }catch(err){
      next(err);
   }
 })
-
-
-
-
-
 
 
 app.listen(3000,()=>{
