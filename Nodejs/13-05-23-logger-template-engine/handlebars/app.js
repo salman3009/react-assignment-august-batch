9653383e54dd24452ffd7b52952ec1bf8bd7d5bc
salman3009 <salman444g@gmail.com>
2023-05-13 13:42:52 +0530

    code

--- Nodejs/13-05-23-logger-template-engine/handlebars/app.js
+++ Nodejs/13-05-23-logger-template-engine/handlebars/app.js
@@ -9,6 +9,15 @@ app.get('/',(req,res)=>{
     res.render('index');
 })
 
+app.get('/dynamic',(req,res)=>{
+    let personDetails ={
+        firstName:"akash",
+        age:33,
+        location:"Delhi"
+    };
+    res.render('dynamic',{personData:personDetails});
+})
+
 app.listen(3000,()=>{
     console.log("server is running on 3000");
 })
