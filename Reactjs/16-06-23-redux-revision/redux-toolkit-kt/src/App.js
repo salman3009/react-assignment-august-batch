515d27f5e58969714257096445c900fe5b5f9e2c
salman3009 <salman444g@gmail.com>
2023-06-16 19:34:11 +0530

    ccode

--- Reactjs/16-06-23-redux-revision/redux-toolkit-kt/src/App.js
+++ Reactjs/16-06-23-redux-revision/redux-toolkit-kt/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import { useSelector } from 'react-redux'
+import { useSelector,useDispatch } from 'react-redux';
+import { increment ,incrementByAmount} from './counterSlice'
+import { register} from './loginSlice'
+
+
+
 
 function App() {
 
+  const dispatch = useDispatch();
   const {value} = useSelector((state) => state.counter)
   const {email} = useSelector((state) => state.login)
+
+  const onChangeHandler=(event)=>{
+    dispatch(register({email:event.target.value}))
+  }
 
   return (
     <div className="App">
@@ -15,6 +24,22 @@ function App() {
       <h2>
         {email}
       </h2>
+      <div>
+      <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(incrementByAmount(200))}
+        >
+          Amount
+        </button>
+        <br/>
+         <input type="text" onChange={onChangeHandler}/>
+      </div>
     </div>
   );
 }
--- Reactjs/16-06-23-redux-revision/redux-toolkit-kt/src/loginSlice.js
+++ Reactjs/16-06-23-redux-revision/redux-toolkit-kt/src/loginSlice.js
@@ -10,12 +10,14 @@ export const loginSlice = createSlice({
   name: 'login',
   initialState,
   reducers: {
-    register: (state) => {
+    register: (state,action) => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1
+      console.log(action.payload)
+      state = {...state,email:action.payload.email};
+      return state;
     },
     login: (state) => {
       state.value -= 1
